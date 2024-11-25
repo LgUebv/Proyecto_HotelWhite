87af2e06aa5719821f573a514274357fcbb8af8a
Jose Vega <lguebv@gmail.com>
2024-11-25 13:49:15 -0600

    se agregaron el manejador de reservaciones

---
+++ Manejador/ManejadorReservaciones.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AccesoDatos;
+
+namespace Manejador
+{
+    public class ManejadorReservaciones
+    {
+        Funciones f = new Funciones();
+        public void Guardar(TextBox IDCliente, TextBox IDHabitacion, TextBox Dias)
+        {
+            MessageBox.Show(f.Guardar($"call p_AgregarReservacion ({IDCliente.Text}, {IDHabitacion.Text}, {Dias.Text});"),
+                "Atencion!!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        public void Borrar(int IDR, string dato)
+        {
+            DialogResult rs = MessageBox.Show($"Estas seguro de borrar {dato}?", "Atencion!!", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (rs == DialogResult.Yes)
+            {
+                f.Borrar($"call p_EliminarReservacion ({IDR})");
+                MessageBox.Show("Registro eliminado", "Atencion!!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        public void Modificar(TextBox IDCliente, TextBox IDHabitacion, TextBox Dias, int IDR)
+        {
+            MessageBox.Show(f.Modificar($"call p_EditarReservacion ({IDR}, {IDCliente.Text}, {IDHabitacion.Text}, {Dias.Text});"),
+                "Atencion!!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        DataGridViewButtonColumn Boton(string t, Color fondo)
+        {
+            DataGridViewButtonColumn b = new DataGridViewButtonColumn();
+            b.Text = t;
+            b.UseColumnTextForButtonValue = true;
+            b.FlatStyle = FlatStyle.Popup;
+            b.DefaultCellStyle.BackColor = fondo;
+            b.DefaultCellStyle.ForeColor = Color.White;
+            return b;
+        }
+        public void Mostrar(DataGridView tabla, string filtro)
+        {
+            tabla.Columns.Clear();
+            tabla.DataSource = f.Mostrar($"select * from Vista_Reservaciones where Nombre_Cliente like '%{filtro}%'", "reservaciones").Tables[0];
+            tabla.Columns.Insert(8, Boton("Borrar", Color.Red));
+            tabla.Columns.Insert(9, Boton("Modificar", Color.Green));
+            tabla.AutoResizeColumns();
+            tabla.AutoResizeRows();
+        }
+    }
+}
--- ProyectoHotel/frmReservaciones.Designer.cs
+++ ProyectoHotel/frmReservaciones.Designer.cs
@@ -30,18 +30,18 @@
         {
             this.txtDias = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.dtgvClientes = new System.Windows.Forms.DataGridView();
-            this.pListas = new System.Windows.Forms.Panel();
-            this.dtgvHabitaciones = new System.Windows.Forms.DataGridView();
             this.btnAgregar = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dtgvClientes)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dtgvHabitaciones)).BeginInit();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCliente = new System.Windows.Forms.TextBox();
+            this.txtHabitacion = new System.Windows.Forms.TextBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtDias
             // 
             this.txtDias.Location = new System.Drawing.Point(461, 19);
-            this.txtDias.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtDias.Margin = new System.Windows.Forms.Padding(2);
             this.txtDias.Multiline = true;
             this.txtDias.Name = "txtDias";
             this.txtDias.Size = new System.Drawing.Size(81, 28);
@@ -57,62 +57,80 @@
             this.label1.TabIndex = 11;
             this.label1.Text = "Dias";
             // 
-            // dtgvClientes
-            // 
-            this.dtgvClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtgvClientes.Location = new System.Drawing.Point(9, 8);
-            this.dtgvClientes.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.dtgvClientes.Name = "dtgvClientes";
-            this.dtgvClientes.RowHeadersWidth = 51;
-            this.dtgvClientes.Size = new System.Drawing.Size(340, 145);
-            this.dtgvClientes.TabIndex = 10;
-            // 
-            // pListas
-            // 
-            this.pListas.AutoScroll = true;
-            this.pListas.Location = new System.Drawing.Point(368, 69);
-            this.pListas.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.pListas.Name = "pListas";
-            this.pListas.Size = new System.Drawing.Size(293, 236);
-            this.pListas.TabIndex = 9;
-            this.pListas.Paint += new System.Windows.Forms.PaintEventHandler(this.pListas_Paint);
-            // 
-            // dtgvHabitaciones
-            // 
-            this.dtgvHabitaciones.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtgvHabitaciones.Location = new System.Drawing.Point(9, 160);
-            this.dtgvHabitaciones.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.dtgvHabitaciones.Name = "dtgvHabitaciones";
-            this.dtgvHabitaciones.RowHeadersWidth = 51;
-            this.dtgvHabitaciones.Size = new System.Drawing.Size(340, 145);
-            this.dtgvHabitaciones.TabIndex = 8;
-            // 
             // btnAgregar
             // 
-            this.btnAgregar.Location = new System.Drawing.Point(561, 7);
-            this.btnAgregar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnAgregar.Location = new System.Drawing.Point(381, 148);
+            this.btnAgregar.Margin = new System.Windows.Forms.Padding(2);
             this.btnAgregar.Name = "btnAgregar";
             this.btnAgregar.Size = new System.Drawing.Size(100, 48);
             this.btnAgregar.TabIndex = 7;
             this.btnAgregar.Text = "AGREGAR";
             this.btnAgregar.UseVisualStyleBackColor = true;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 19);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Cliente:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 62);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Habitacion";
+            // 
+            // txtCliente
+            // 
+            this.txtCliente.Location = new System.Drawing.Point(88, 16);
+            this.txtCliente.Margin = new System.Windows.Forms.Padding(2);
+            this.txtCliente.Multiline = true;
+            this.txtCliente.Name = "txtCliente";
+            this.txtCliente.Size = new System.Drawing.Size(81, 28);
+            this.txtCliente.TabIndex = 15;
+            // 
+            // txtHabitacion
+            // 
+            this.txtHabitacion.Location = new System.Drawing.Point(88, 59);
+            this.txtHabitacion.Margin = new System.Windows.Forms.Padding(2);
+            this.txtHabitacion.Multiline = true;
+            this.txtHabitacion.Name = "txtHabitacion";
+            this.txtHabitacion.Size = new System.Drawing.Size(81, 28);
+            this.txtHabitacion.TabIndex = 16;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(205, 148);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(100, 48);
+            this.btnCancelar.TabIndex = 17;
+            this.btnCancelar.Text = "CANCELAR";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
             // frmReservaciones
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(682, 321);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.txtHabitacion);
+            this.Controls.Add(this.txtCliente);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.txtDias);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.dtgvClientes);
-            this.Controls.Add(this.pListas);
-            this.Controls.Add(this.dtgvHabitaciones);
             this.Controls.Add(this.btnAgregar);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmReservaciones";
             this.Text = "frmReservaciones";
-            ((System.ComponentModel.ISupportInitialize)(this.dtgvClientes)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dtgvHabitaciones)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,9 +140,11 @@
 
         private System.Windows.Forms.TextBox txtDias;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataGridView dtgvClientes;
-        private System.Windows.Forms.Panel pListas;
-        private System.Windows.Forms.DataGridView dtgvHabitaciones;
         private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCliente;
+        private System.Windows.Forms.TextBox txtHabitacion;
+        private System.Windows.Forms.Button btnCancelar;
     }
 }
